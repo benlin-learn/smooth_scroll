a34ec293cb765cd4da8916b16a08b63bdf36c5e2
wide8550 <wide8550@gmail.com>
2021-03-05 12:42:35 +0800

    using window.scroll methods

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,38 @@
-// option2
-$('.navbar a').on('click', function (e) {
-  // console.log(this);
-  console.log(this.hash);
-  if (this.hash !== '') {
+// option 2
+// $('.navbar a').on('click', function (e) {
+//   // console.log(this);
+//   console.log(this.hash);
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     console.log($(hash).offset().top);
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       800
+//     );
+//   }
+// });
+
+// option 3 - window.scroll
+const links = document.querySelectorAll('.navbar a');
+links.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    const el = this.hash;
     e.preventDefault();
-
-    const hash = this.hash;
-
-    console.log($(hash).offset().top);
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      800
-    );
-  }
+    // console.log(
+    //   window.pageYOffset +
+    //     document.querySelector(el).getBoundingClientRect().top
+    // );
+    window.scroll({
+      top:
+        window.pageYOffset +
+        document.querySelector(el).getBoundingClientRect().top,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
 });
